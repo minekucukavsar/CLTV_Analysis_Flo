b9e1b1c328b93b344edf6da3c687bfb8c591f76c
mine <64801738+minekucukavsar@users.noreply.github.com>
2022-07-07 12:36:33 +0300

    Add files via upload

---
+++ flo_cltv.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import datetime as dt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from sklearn.preprocessing import MinMaxScaler
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.options.mode.chained_assignment = None
+import warnings
+warnings.filterwarnings('error')
+
+df_ = pd.read_csv(r"C:\Users\hp\PycharmProjects\pythonProject2\9_odevler\flo_data_20k.csv")
+df = df_.copy()
+
+#Define the outlier_thresholds and replace_with_thresholds functions needed to suppress outliers.
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = round(low_limit,0)
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = round(up_limit,0)
+
+
+columns = ["order_num_total_ever_online", "order_num_total_ever_offline", "customer_value_total_ever_offline","customer_value_total_ever_online"]
+for col in columns:
+    replace_with_thresholds(df, col)
+
+#Omnichannel means that customers shop from both online and offline platforms.
+#New variables have been created for the total number of purchases and spending of each customer.
+df["order_num_total"] = df["order_num_total_ever_online"] + df["order_num_total_ever_offline"]
+df["customer_value_total"] = df["customer_value_total_ever_offline"] + df["customer_value_total_ever_online"]
+
+date_columns = df.columns[df.columns.str.contains("date")]
+df[date_columns] = df[date_columns].apply(pd.to_datetime)
+
+#Creating CLTV structure
+#Deciding the analysis date
+df["last_order_date"].max() # 2021-05-30
+analysis_date = dt.datetime(2021,6,1)
+
+
+# Creating new CLTV DataFrame it includes;customer_id, recency_cltv_weekly, T_weekly, frequency ve monetary_cltv_avg .
+cltv_df = pd.DataFrame()
+cltv_df["customer_id"] = df["master_id"]
+cltv_df["recency_cltv_weekly"] = ((df["last_order_date"]- df["first_order_date"]).astype('timedelta64[D]')) / 7
+analysis_date-df["last_order_date"].max()
+cltv_df["T_weekly"] = ((analysis_date - df["first_order_date"]).astype('timedelta64[D]'))/7
+cltv_df["frequency"] = df["order_num_total"]
+cltv_df["monetary_cltv_avg"] = df["customer_value_total"] / df["order_num_total"]
+
+cltv_df.head()
+
+#Creating BG/NBD, Gamma-Gamma models and calculation CLTV for 6 months.
+
+#BG/NBD model
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+bgf.fit(cltv_df['frequency'],cltv_df['recency_cltv_weekly'],cltv_df['T_weekly'])
+
+# Expected purchase from cumtomers in 3 months
+cltv_df["exp_sales_3_month"] = bgf.predict(4*3,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency_cltv_weekly'],
+                                       cltv_df['T_weekly'])
+
+# Expected purchase from cumtomers in 6 months
+cltv_df["exp_sales_6_month"] = bgf.predict(4*6,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency_cltv_weekly'],
+                                       cltv_df['T_weekly'])
+
+#Fitting Gamma-Gamma model. Calculation exp_average_value.
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+ggf.fit(cltv_df['frequency'], cltv_df['monetary_cltv_avg'])
+cltv_df["exp_average_value"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                cltv_df['monetary_cltv_avg'])
+cltv_df.head()
+
+#Calculation CLTV for 6 months
+cltv = ggf.customer_lifetime_value(bgf,
+                                   cltv_df['frequency'],
+                                   cltv_df['recency_cltv_weekly'],
+                                   cltv_df['T_weekly'],
+                                   cltv_df['monetary_cltv_avg'],
+                                   time=6,
+                                   freq="W",
+                                   discount_rate=0.01)
+cltv_df["cltv"] = cltv
+
+
+cltv_df.head()
+
+
+
